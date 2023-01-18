2631e300112e0ca3ca7ccbc5f32fcbcc37b285a9
hitendrasinh-Parmar <hitrajput098@gmail.com>
2023-01-18 20:16:53 +0530

    remove .env file from git

--- App.tsx
+++ App.tsx
@@ -14,6 +14,7 @@ const App = () => {
     <NavigationContainer ref={navigationRef}>
       <ThemeProvider>
         <Provider store={store}>
+
           {/* <AppProvider> */}
           <RootNavigator />
           {/* </AppProvider> */}
